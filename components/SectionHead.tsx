cfbb0a3373332c5b9b2829671c5871b47e3994e8
Reyad-Mousa <99836425+Reyad-Mousa@users.noreply.github.com>
2024-03-10 04:00:10 +0200

    Update SectionHead.tsx

--- components/SectionHead.tsx
+++ components/SectionHead.tsx
@@ -3,7 +3,11 @@ type sectionHeadProps = {
   children: React.ReactNode;
 };
 const SectionHead = ({ children }: sectionHeadProps) => {
-  return <h2 className="font-bold text-2xl sm:text-3xl  mb-16">{children}</h2>;
+  return (
+    <h2 className="font-bold text-center text-2xl sm:text-3xl  mb-16">
+      {children}
+    </h2>
+  );
 };
 
 export default SectionHead;
