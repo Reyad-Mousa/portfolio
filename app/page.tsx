7a8a94f5d2b95933e60670039f1119c9ebb734d5
Reyad-Mousa <99836425+Reyad-Mousa@users.noreply.github.com>
2024-03-13 18:14:24 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import About from "@/components/About";
 import Intro from "@/components/Intro";
 import Project from "@/components/Project";
-import Skills from "@/components/Skils";
-import Image from "next/image";
-
+import Skills from "@/components/Skills";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-16">
