b3d9580e5d93a3ad306d31a1970c4f53792924ac
Reyad-Mousa <99836425+Reyad-Mousa@users.noreply.github.com>
2024-03-13 17:56:33 +0200

    Create Skils.tsx
    create skills page

---
+++ components/Skils.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React from "react";
+import SectionHead from "./SectionHead";
+import { skillsData } from "@/lib/data";
+import { useActiveSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
+
+const Skills = () => {
+  const { ref } = useActiveSectionInView("Skills");
+
+  const fadeInAnimationVariants = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: (index: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.05 * index,
+      },
+    }),
+  };
+  return (
+    <section className=" w-60  sm:w-1/2" ref={ref} id="skills">
+      <SectionHead>Skills</SectionHead>
+      <ul className="flex flex-wrap gap-2 text-lg text-gray-800">
+        {skillsData.map((skill, index) => (
+          <motion.li
+            className="bg-white border border-black/[0.1] px-5 py-3 rounded-xl"
+            key={index}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Skills;
